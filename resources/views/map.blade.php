4c69b0b5435d9c2f32852afb5f85d298079c7882
ratihesti <ratih4859@gmail.com>
2026-04-23 06:46:42 +0700

    update pgwl acara 6 API GEOJSON SERVICES

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -228,5 +228,94 @@
 
             drawnItems.addLayer(layer);
         });
+
+        // GeoJSON Point
+        var points = L.geoJSON(null, {
+            // Style
+
+            // onEachFeature
+            onEachFeature: function(feature, layer) {
+                // variable popup content
+                var popup_content = "Nama: " + feature.properties.name + "<br>" +
+                    "Deskripsi: " + feature.properties.description + "<br>" +
+                    "created_at: " + feature.properties.created_at + "<br>" +
+                    "updated_at: " + feature.properties.updated_at;
+
+                layer.on({
+                    click: function(e) {
+                        points.bindPopup(popup_content);
+                    },
+                });
+            },
+
+        });
+        $.getJSON("{{ route('geojson_points') }}", function(data) {
+            points.addData(data);
+            map.addLayer(points);
+        });
+
+        // GeoJSON Polyline
+        var polylines = L.geoJSON(null, {
+            // Style
+
+            // onEachFeature
+            onEachFeature: function(feature, layer) {
+                // variable popup content
+                var popup_content = "Nama: " + feature.properties.name + "<br>" +
+                    "Deskripsi: " + feature.properties.description + "<br>" +
+                    "created_at: " + feature.properties.created_at + "<br>" +
+                    "updated_at: " + feature.properties.updated_at;
+
+                layer.on({
+                    click: function(e) {
+                        polylines.bindPopup(popup_content);
+                    },
+                });
+            },
+
+        });
+        $.getJSON("{{ route('geojson_polylines') }}", function(data) {
+            polylines.addData(data);
+            map.addLayer(polylines);
+        });
+
+        // GeoJSON Polygon
+        var polygons = L.geoJSON(null, {
+            // Style
+
+            // onEachFeature
+            onEachFeature: function(feature, layer) {
+                // variable popup content
+                var popup_content = "Nama: " + feature.properties.name + "<br>" +
+                    "Deskripsi: " + feature.properties.description + "<br>" +
+                    "created_at: " + feature.properties.created_at + "<br>" +
+                    "updated_at: " + feature.properties.updated_at;
+
+                layer.on({
+                    click: function(e) {
+                        polygons.bindPopup(popup_content);
+                    },
+                });
+            },
+
+        });
+        $.getJSON("{{ route('geojson_polygons') }}", function(data) {
+            polygons.addData(data);
+            map.addLayer(polygons);
+        });
+
+        // Control Layer
+        var baseMaps = {
+
+        };
+
+        var overlayMaps = {
+            "Point": points,
+            "Polyline": polylines,
+            "Polygon": polygons,
+        };
+
+        var controllayer = L.control.layers(baseMaps, overlayMaps);
+        controllayer.addTo(map);
     </script>
 @endsection
